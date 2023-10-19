fe5eb7cef130e024751fcdd1bd159b7a34d99498
akshayar <akshaya.rawat@gmail.com>
2023-10-19 20:28:54 +0530

    SpringBoot sample for Kafka

--- msk-kafka-springboot-producer/src/main/java/com/aws/samples/Application.java
+++ msk-kafka-springboot-producer/src/main/java/com/aws/samples/Application.java
@@ -64,6 +64,8 @@ public class Application
         props.put(ProducerConfig.MAX_IN_FLIGHT_REQUESTS_PER_CONNECTION, env.getProperty("spring.kafka.maxInFlightReqPerConnInParallel"));
         props.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
         props.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
+        props.put(ProducerConfig.DELIVERY_TIMEOUT_MS_CONFIG, env.getProperty("spring.kafka.deliveryTimeoutMs"));
+        props.put(ProducerConfig.REQUEST_TIMEOUT_MS_CONFIG, env.getProperty("spring.kafka.producerRetryTimeOutMilliSeconds"));
         return props;
     }
     @Bean
