9080f464b29b8be70b2c9f5a3ae3c566b26a0941
akshayar <akshaya.rawat@gmail.com>
2023-11-06 19:18:30 +0530

    MM2 monitoring.

--- admin-ui/deploy.sh
+++ admin-ui/deploy.sh
@@ -2,13 +2,13 @@
 ## Parameters
 export ECS_CLUSTER_NAME=ForMSK
 export REGION=ap-south-1
-export SOURCE_KAFKA_CLUSTER_BOOTSTRAP="b-1.source.yjtfqf.c4.kafka.ap-south-1.amazonaws.com:9092,b-2.source.yjtfqf.c4.kafka.ap-south-1.amazonaws.com:9092"
-export DESTINATION_KAFKA_CLUSTER_BOOTSTRAP="b-1.target.oumzco.c4.kafka.ap-south-1.amazonaws.com:9092,b-2.target.oumzco.c4.kafka.ap-south-1.amazon:9092"
-export MSKCluster1SG=sg-09642b7a0ad7e5bf9
-export MSKCluster2SG=sg-09642b7a0ad7e5bf9
-export ECS_ALB_VPC=vpc-01be4940d7ee23da5
-export ECS_ALB_PublicSubnet1=subnet-0d85538e799c33c9c
-export ECS_ALB_PublicSubnet2=subnet-03dfb01c582465083
+export SOURCE_KAFKA_CLUSTER_BOOTSTRAP=""
+export DESTINATION_KAFKA_CLUSTER_BOOTSTRAP=""
+export MSKCluster1SG=sg-0bc9fa1111111
+export MSKCluster2SG=sg-0b909a2222222
+export ECS_ALB_VPC=vpc-0111111111
+export ECS_ALB_PublicSubnet1=subnet-0d1111111111
+export ECS_ALB_PublicSubnet2=subnet-032222222222
 
 
 echo "Fetching ECS cluster ARN"
@@ -31,4 +31,5 @@ aws cloudformation deploy --template-file akhq-ecs-deploy.yml \
    PublicSubnet1=${ECS_ALB_PublicSubnet1} \
    PublicSubnet2=${ECS_ALB_PublicSubnet2} \
    MSKCluster1SG=${MSKCluster1SG} \
-   MSKCluster2SG=${MSKCluster2SG}
+   MSKCluster2SG=${MSKCluster2SG} \
+   DesiredCount=1
--- mirror-maker/create-source-sink-clusters.sh
+++ mirror-maker/create-source-sink-clusters.sh
@@ -1,10 +1,47 @@
 #!/bin/bash
+function getClusterInformation() {
+  stack_name=$1
+  aws cloudformation describe-stacks --stack-name ${stack_name} --region ${REGION} \
+   --query "Stacks[0].Outputs" --output text --no-cli-pager
+
+  MSK_CLUSTER=$(aws kafka list-clusters --region ${REGION} --cluster-name-filter ${stack_name} \
+    --region ${REGION} --query "ClusterInfoList[0].{ClusterArn:ClusterArn}" --output text --no-cli-pager)
+
+  MSK_CLUSTER_SG=$(aws kafka describe-cluster --cluster-arn ${MSK_CLUSTER} --region ${REGION} \
+    --region ${REGION} --query "ClusterInfo.BrokerNodeGroupInfo.SecurityGroups" --output text --no-cli-pager )
+
+  export MSK_CLUSTER_BOOTSTRAP=$(aws kafka get-bootstrap-brokers --cluster-arn ${MSK_CLUSTER} \
+    --region ${REGION} --query "BootstrapBrokerString" --output text --no-cli-pager)
+
+    #echo "${MSK_CLUSTER}"
+    echo "${MSK_CLUSTER_SG}"
+    echo "${MSK_CLUSTER_BOOTSTRAP}"
+    echo "#############"
+}
+
+function create_cluster() {
+  stack_name=$1
+  cluster_version=$2
+  echo "Deploying MSK Cluster ${stack_name} with version ${cluster_version}"
+  aws cloudformation deploy --template-file msk-provisioned-cluster.yml \
+     --capabilities CAPABILITY_NAMED_IAM CAPABILITY_AUTO_EXPAND \
+     --stack-name ${stack_name}   \
+     --region ${REGION} \
+     --parameter-overrides MSKKafkaVersion=${cluster_version} \
+     VPCId=${VPCId} \
+     Subnet1=${Subnet1} \
+     Subnet2=${Subnet2} \
+     Subnet3=${Subnet3} \
+     InstanceType=${InstanceType} \
+     MSKPublisherSG=${MSKPublisherSG}
+}
 export REGION=ap-south-1
-export MSKSourceKafkaVersion=2.8.0
-export VPCId=vpc-01be4940d7ee23da5
-export Subnet1=subnet-03dfb01c582465083
-export Subnet2=subnet-0d85538e799c33c9c
-export Subnet3=subnet-069dc87ebf9750664
+export MSKSourceKafkaVersion=2.2.1
+export MSKTargetKafkaVersion=2.8.2.tiered
+export VPCId=vpc-01111111
+export Subnet1=subnet-03111111
+export Subnet2=subnet-0d222222
+export Subnet3=subnet-06333333
 export InstanceType=kafka.m5.large
 echo "Creating MSK client SG"
 aws ec2 create-security-group \
@@ -17,31 +54,15 @@ export MSKPublisherSG=$(aws ec2 describe-security-groups --region ${REGION}\
         --output text)
 
 echo "MSK Publisher SG created with ID: ${MSKPublisherSG}"
-echo "Deploying MSK Source Cluster "
-aws cloudformation deploy --template-file msk-provisioned-cluster.yml \
-   --capabilities CAPABILITY_NAMED_IAM CAPABILITY_AUTO_EXPAND \
-   --stack-name msk-source-cluster   \
-   --region ${REGION} \
-   --parameter-overrides MSKSourceKafkaVersion=${MSKSourceKafkaVersion} \
-   VPCId=${VPCId} \
-   Subnet1=${Subnet1} \
-   Subnet2=${Subnet2} \
-   Subnet3=${Subnet3} \
-   InstanceType=${InstanceType} \
-   MSKPublisherSG=${MSKPublisherSG} \
-   &
 
+create_cluster msk-source-cluster ${MSKSourceKafkaVersion} &
+create_cluster msk-destination-cluster ${MSKTargetKafkaVersion}
 
-echo "Deploying MSK Destination Cluster "
-aws cloudformation deploy --template-file msk-provisioned-cluster.yml \
-   --capabilities CAPABILITY_NAMED_IAM CAPABILITY_AUTO_EXPAND \
-   --stack-name msk-destination-cluster   \
-   --region ${REGION} \
-   --parameter-overrides MSKSourceKafkaVersion=${MSKSourceKafkaVersion} \
-   VPCId=${VPCId} \
-   Subnet1=${Subnet1} \
-   Subnet2=${Subnet2} \
-   Subnet3=${Subnet3} \
-   InstanceType=${InstanceType} \
-   MSKPublisherSG=${MSKPublisherSG} \
-   &
+## Check for cloudformation stack status and wait
+#aws cloudformation wait stack-create-complete --stack-name msk-source-cluster --region ${REGION} --output text --no-cli-pager
+#aws cloudformation wait stack-create-complete --stack-name msk-destination-cluster --region ${REGION} --output text --no-cli-pager
+
+## Query cloudformation stack output using cli
+getClusterInformation msk-source-cluster
+getClusterInformation msk-destination-cluster
+
