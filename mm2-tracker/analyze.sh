584d4d682afe933d21b5ea9d1fcebe2b05f7e237
akshayar <akshaya.rawat@gmail.com>
2024-01-03 21:05:16 +0530

    MM2 tracker.

--- mm2-tracker/analyze.sh
+++ mm2-tracker/analyze.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 echo "Compiling and running the program"
-mvn clean compile exec:java -Dexec.mainClass="org.example.Main" -Dexec.args="source_topic_offset.txt destination_topic_offset.txt source_offset_sync.txt"
+mvn -q clean compile exec:java -Dexec.mainClass="org.example.Main" -Dexec.args="source_topic_offset.txt destination_topic_offset.txt source_offset_sync.txt"
