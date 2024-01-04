870e58b2bbd479c3960771de0dbeb55626ce8379
akshayar <akshaya.rawat@gmail.com>
2024-01-04 09:13:40 +0530

    MM2 tracker.

--- mm2-tracker/check-mm2-sync.sh
+++ mm2-tracker/check-mm2-sync.sh
@@ -4,6 +4,7 @@ SOURCE_KAFKA_URL=$1
 DESTINATION_KAFKA_URL=$2
 offset_topic=$3
 #offset_topic=mm2-offset-syncs..internal
-./probe-kafka.sh source ${SOURCE_KAFKA_URL} ${offset_topic} &
 ./probe-kafka.sh destination ${DESTINATION_KAFKA_URL} &
+./probe-kafka.sh source ${SOURCE_KAFKA_URL} ${offset_topic} &
+
 wait
