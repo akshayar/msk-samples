5b0e631fab70f7a10df38c9357aba59ca0b2ff88
akshayar <akshaya.rawat@gmail.com>
2024-04-06 16:20:40 +0530

    MSK Connect deployment script.

--- mirror-maker/msk-connect/deploy-with-cft.sh
+++ mirror-maker/msk-connect/deploy-with-cft.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 ## Parameters
 export REGION=ap-south-1
-export SOURCE_KAFKA_CLUSTER_BOOTSTRAP="b-3.msksourcecluster.n6yhmp.c4.kafka.ap-south-1.amazonaws.com:9092,b-2.msksourcecluster.n6yhmp.c4.kafka.ap-south-1.amazonaws.com:9092,b-1.msksourcecluster.n6yhmp.c4.kafka.ap-south-1.amazonaws.com:9092"
-export DESTINATION_KAFKA_CLUSTER_BOOTSTRAP="b-1.mskdestinationcluster.01l0zn.c4.kafka.ap-south-1.amazonaws.com:9092,b-3.mskdestinationcluster.01l0zn.c4.kafka.ap-south-1.amazonaws.com:9092,b-2.mskdestinationcluster.01l0zn.c4.kafka.ap-south-1.amazonaws.com:9092"
-export SOURCE_CLUSTER_SG=sg-054b423ef3b89cf5a
-export DESTINATION_CLUSTER_SG=sg-0878c71b5594a9235
-export SUBNET_1=subnet-03dfb01c582465083
-export SUBNET_2=subnet-0d85538e799c33c9c
+export SOURCE_KAFKA_CLUSTER_BOOTSTRAP=""
+export DESTINATION_KAFKA_CLUSTER_BOOTSTRAP=""
+export SOURCE_CLUSTER_SG=sg-0b111111fbba11e11
+export DESTINATION_CLUSTER_SG=sg-0bbbb11111bb11111
+export SUBNET_1=subnet-0bbbb11111bb11111
+export SUBNET_2=subnet-0bbbb11111bb11111
 export MM2_CUSTOM_PLUGIN_ARN=
 export MM2_WORKER_CONFIG_ARN=
 export TASK_MAX=2
